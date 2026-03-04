611ecc0d32ed1631158e798555b1100f305c4de8
JCmontero22 <johan00712@gmail.com>
2026-03-04 00:41:48 -0500

    Correccion credenciales

--- config/configDB.php
+++ config/configDB.php
@@ -5,14 +5,14 @@
     DEFINE('DB_PASS', '');
     DEFINE('DB_NAME', 'jm_productos'); */
 
-    DEFINE('DB_HOST', 'localhost');
+    /* DEFINE('DB_HOST', 'localhost');
     DEFINE('DB_USER', 'jmonteroDev');
     DEFINE('DB_PASS', 'stratovarius');
-    DEFINE('DB_NAME', 'jm_productos');
+    DEFINE('DB_NAME', 'jm_productos'); */
 
     /* PRODUCCION */
-    /* define('DB_HOST', '127.0.0.1');
+    define('DB_HOST', '127.0.0.1');
     define('DB_NAME', 'jcmonter_productos_aseo');
     define('DB_USER', 'jcmonter_dev94');
-    define('DB_PASS', 'Stratovarius94@'); */
+    define('DB_PASS', 'Stratovarius94@');
    
