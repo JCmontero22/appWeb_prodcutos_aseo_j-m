1e4ba070827667ea2baf7d1c51ebe7557e3092cb
Johan Montero <johan00712@gmail.com>
2026-04-11 08:37:29 -0500

    Actualizacion htaccess

--- config/configDB.php
+++ config/configDB.php
@@ -1,9 +1,9 @@
 <?php 
     
-    DEFINE('DB_HOST', 'localhost');
+    /* DEFINE('DB_HOST', 'localhost');
     DEFINE('DB_USER', 'root');
     DEFINE('DB_PASS', '');
-    DEFINE('DB_NAME', 'jm_productos');
+    DEFINE('DB_NAME', 'jm_productos'); */
 
     /* DEFINE('DB_HOST', 'localhost');
     DEFINE('DB_USER', 'jmonteroDev');
@@ -11,8 +11,8 @@
     DEFINE('DB_NAME', 'jm_productos'); */
 
     /* PRODUCCION */
-    /* define('DB_HOST', '127.0.0.1');
+    define('DB_HOST', '127.0.0.1');
     define('DB_NAME', 'jcmonter_productos_aseo');
     define('DB_USER', 'jcmonter_dev94');
-    define('DB_PASS', 'Stratovarius94@'); */
+    define('DB_PASS', 'Stratovarius94@');
    
