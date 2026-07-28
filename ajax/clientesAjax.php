15c7201d92996394e6de5e73a3eb632f06cb48b2
Johan Montero <johan00712@gmail.com>
2026-07-28 11:59:32 -0500

    Prueba15

--- ajax/clientesAjax.php
+++ ajax/clientesAjax.php
@@ -1,31 +1,6 @@
-<?php
+<?php 
 
-require_once('../controller/listadoClientesController.php');
+    require_once('../controller/listadoClientesController.php');
 
-$listadoClientes = new listadoClientesController();
-
-$resultado = $listadoClientes->listarClientes();
-
-$cliente = $resultado['data'][1];
-
-foreach ($cliente as $campo => $valor) {
-
-    echo "<hr>";
-    echo "<strong>$campo</strong><br>";
-
-    var_dump($valor);
-
-    if (is_string($valor)) {
-
-        echo "HEX: ".bin2hex($valor)."<br>";
-
-        if (json_encode($valor) === false) {
-            echo "<span style='color:red'>ERROR EN ESTE CAMPO</span><br>";
-            echo json_last_error_msg();
-            exit;
-        }
-
-    }
-
-}
-echo "Todos los clientes son válidos";
+    $listadoClientes = new listadoClientesController();
+    echo json_encode($listadoClientes->listarClientes());
