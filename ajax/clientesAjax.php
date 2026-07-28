2f06022a2823356223042c3506ba715f5a1f0b0b
Johan Montero <johan00712@gmail.com>
2026-07-28 11:20:32 -0500

    Prueba4

--- ajax/clientesAjax.php
+++ ajax/clientesAjax.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
 
-    echo "PASO 1<br>";
+echo "PASO 1<br>";
 
-    require_once('../controller/listadoClientesController.php');
+require_once('../controller/listadoClientesController.php');
 
-    echo "PASO 2<br>";
+echo "PASO 2<br>";
 
-    exit;
+$listadoClientes = new listadoClientesController();
 
-    $listadoClientes = new listadoClientesController();
-    echo json_encode($listadoClientes->listarClientes());
+echo "PASO 3<br>";
+
+exit;
