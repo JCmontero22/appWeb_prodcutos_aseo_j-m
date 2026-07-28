6103118f07306612a7055892c8b2fc53d5e4bf7c
Johan Montero <johan00712@gmail.com>
2026-07-28 11:42:03 -0500

    Prueba8

--- ajax/clientesAjax.php
+++ ajax/clientesAjax.php
@@ -1,17 +1,13 @@
 <?php
 
-echo "INICIO";
-
 require_once('../controller/listadoClientesController.php');
-
-echo " - CONTROLLER";
 
 $listadoClientes = new listadoClientesController();
 
-echo " - OBJETO";
-
 $resultado = $listadoClientes->listarClientes();
 
-echo " - RESULTADO";
+echo "<pre>";
+var_dump($resultado);
+echo "</pre>";
 
 exit;
