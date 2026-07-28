a4a78ff253693db8842e33635c3c9ea729192cf1
Johan Montero <johan00712@gmail.com>
2026-07-28 11:43:55 -0500

    Prueba9

--- ajax/clientesAjax.php
+++ ajax/clientesAjax.php
@@ -7,7 +7,13 @@ $listadoClientes = new listadoClientesController();
 $resultado = $listadoClientes->listarClientes();
 
 echo "<pre>";
-var_dump($resultado);
+var_dump(json_encode($resultado));
 echo "</pre>";
+
+echo "<br><br>";
+
+echo json_last_error();
+echo "<br>";
+echo json_last_error_msg();
 
 exit;
